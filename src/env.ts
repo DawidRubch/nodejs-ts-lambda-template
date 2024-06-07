5b94d5f5efc72d2c945fbd131aef442fdc1db91a
DawidRubch <dawid.niegrebecki@gmail.com>
2024-06-07 14:15:54 +0200

    Added env parsing logic

---
+++ src/env.ts
@@ -0,0 +1,17 @@
+
+import { z } from "zod"
+
+
+
+const envSchema = z.object({
+    PORT: z.string().default("3000"),
+    NODE_ENV: z.string().default("development"),
+    DATABASE_URL: z.string(),
+})
+
+export type Env = z.infer<typeof envSchema>
+
+export const env = envSchema.parse(process.env) as Env
+
+
+
